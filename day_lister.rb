bddd76de67d02cb60ffc0e31dd3647c4395887bf
BenjaGross <Benja.J.Gross@gmail.com>
2016-04-22 15:25:20 -0400

    remove conditional

--- day_lister.rb
+++ day_lister.rb
@@ -2,12 +2,9 @@ require 'date'
 
 def list_days(n: 3, day: 'Monday', start_date: Date.today)
   day = Date.parse(day).wday
-  if start_date.wday == 1
-    map_days(start_date, n)
-  else 
-    next_monday = start_date + ((day - start_date.wday) % 7)
-    map_days(next_monday, n)
-  end
+  next_monday = start_date + ((day - start_date.wday) % 7)
+  map_days(next_monday, n)
+
 end
 
 def map_days(day, n)
